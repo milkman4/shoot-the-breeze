9c74a8601966b99cf5e86b2fdb04004e3d16ea30
MilkMan90 <mlkaufman747@gmail.com>
2016-10-19 15:42:19 -0600

    2 tests passing on user input

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import { shallow, mount, render } from 'enzyme';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 
 import Application from '../lib/components/Application';
+import UserInput from '../lib/components/UserInput';
 
 describe('Application', () => {
 
@@ -11,5 +12,21 @@ describe('Application', () => {
     const wrapper = shallow(<Application />)
     assert.equal(wrapper.type(), 'div');
   });
-
 });
+
+describe('UserInput', () => {
+
+  it('renders as a <form>', () => {
+    const wrapper = shallow(<UserInput />)
+    assert.equal(wrapper.type(), 'form')
+  });
+
+  it('should change the state when user input a message', () => {
+    const wrapper = mount(<UserInput />);
+    const input = wrapper.find('.MessageInputField');
+
+    input.simulate('change', {target: {value: 'hello'} });
+    expect(wrapper.state('draftMessage')).to.equal('hello');
+  });
+
+})
