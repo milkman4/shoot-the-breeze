bb099537db2c5ae9fb025d4c8fd732023104e990
MilkMan90 <mlkaufman747@gmail.com>
2016-10-19 15:59:12 -0600

    added test in application that tests if the submit button renders to the page

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -12,6 +12,17 @@ describe('Application', () => {
     const wrapper = shallow(<Application />)
     assert.equal(wrapper.type(), 'div');
   });
+
+  it('renders a message on the page on click of submit button', () => {
+    const wrapper = mount(<Application />);
+    const input = wrapper.find('.message-input-field');
+
+    input.simulate('change', {target: {value: 'hi'} });
+    wrapper.find('.submit-button').simulate('click');
+    setTimeout(() => {
+      expect(wrapper.text()).to.equal('hi');
+    }, 2000);
+  })
 });
 
 describe('UserInput', () => {
@@ -21,12 +32,16 @@ describe('UserInput', () => {
     assert.equal(wrapper.type(), 'form')
   });
 
+  it('renders as a submit button', () => {
+    const wrapper = shallow(<UserInput />)
+    expect(wrapper.find('.submit-button')).to.have.length(1)
+  });
+
   it('should change the state when user input a message', () => {
     const wrapper = mount(<UserInput />);
-    const input = wrapper.find('.MessageInputField');
+    const input = wrapper.find('.message-input-field');
 
     input.simulate('change', {target: {value: 'hello'} });
     expect(wrapper.state('draftMessage')).to.equal('hello');
   });
-
 })
