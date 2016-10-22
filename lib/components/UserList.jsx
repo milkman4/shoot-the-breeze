07229451c460c20206c31fa64420708491f21518
MilkMan90 <mlkaufman747@gmail.com>
2016-10-22 17:35:14 -0600

    user-list-style

--- lib/components/UserList.jsx
+++ lib/components/UserList.jsx
@@ -3,7 +3,6 @@ import {SingleUser} from './SingleUser.jsx';
 import {countBy} from 'lodash';
 import {map, toArray} from 'lodash';
 
-
 export const UserList = (userList) => {
   let userListArray = userList.userList
   let displayUsers = userListArray.map(user => <SingleUser {...user} key={user.uid}/>)
